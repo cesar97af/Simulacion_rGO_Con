b9338f63765f9e6742c6e46a573e7a3b7e60420f
César Augusto Fernández García <cesar97af@gmail.com>
2026-05-20 21:05:06 -0500

    Cambios por nuevo entorno virtual

--- Intento de simulación.py
+++ Intento de simulación.py
@@ -1,7 +1,7 @@
-#import kwant
-#import kwant.continuum
+import kwant
+import kwant.continuum
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
-#import tinyarray
+import tinyarray
 from scipy import constants
